f12aace9a17299131adb0f73bf885627f39b55d6
Samarth Kishor <samarthkishor1@gmail.com>
2017-08-25 00:10:43 -0400

    Saves the explanation paragraph in a plain text file

--- apod.js
+++ apod.js
@@ -1,5 +1,7 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare();
+const fs = require('fs');
+
 
 // format the current date
 const today = new Date();
@@ -8,15 +10,16 @@ const month = today.getMonth();
 const year = today.getFullYear();
 const date = `${month}-${day}-${year}`;
 
+
 // start the web scraper
 nightmare
 .goto('https://apod.nasa.gov/apod/')
 .wait('body > p')
 .evaluate(function () {
-    return document.querySelector('body > p').textContent;
+    return document.querySelector('body > p').textContent.trim();
 })
 .then(function (result) {
-    console.log(result);
+    fs.appendFile('explanations.txt', date + ', ' + result + '\n\n');
     return nightmare
         .click('body > center:nth-child(1) > p:nth-child(3) > a > img')
         .wait(5000)
